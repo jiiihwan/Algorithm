7cb379675c1774fd51c50cbc0f5fdbf217ed1e58
jihwan <yjh2353693@naver.com>
2025-07-14 22:30:01 +0900

    [S4] fail

---
+++ 3. 연결리스트/1158. 요세푸스 문제/1158.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N,K;
+    cin >> N >> K;
+
+    vector<int> V,ans;
+    for(int i = 1 ; i < N ; i++){
+        V.push_back(i);
+    }
+
+    for(int i = 0 ; ans.size() < N ; i++){
+        if(i % K == K -1){
+            ans.push_back(V[i]);
+        }
+        else
+            V.push_back(V[i]);
+    }
+    
+    cout << "<";
+    for(int i = 0 ; i < N ; i++){
+        if(i == N)
+            cout << ans[i];
+        else   
+            cout << ans[i] << ", ";
+    }
+    cout << ">";
+
+}
