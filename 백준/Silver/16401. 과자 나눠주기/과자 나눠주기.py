4dd1b77cd4bf84f619f6421319820e33f8e36e0c
jiiihwan <137675779+jiiihwan@users.noreply.github.com>
2025-10-29 16:33:58 +0900

    [Silver II] Title: 과자 나눠주기, Time: 4936 ms, Memory: 145152 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+m,n = map(int,input().split())
+snack = list(map(int, input().split()))
+
+st,en = 1, max(snack)
+while st <= en:
+    mid = (st+en)//2 #실험할 길이
+    cnt = 0 #mid길이로 잘랐을때 몇개가 나오는지 담을 변수
+    for i in snack: #진짜 잘라보는 과정
+        cnt += i//mid
+    if cnt >= m: #더 많이 나왔다면 더 길게 잘라도 된다.
+        st = mid + 1
+    else:
+        en = mid - 1
+        
+print(en)
+
