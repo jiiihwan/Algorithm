f15dfcb65dc6fef180e80f7630390231eb5ba3e9
jihwan <yjh2353693@naver.com>
2025-11-21 16:36:53 +0900

    success. 25m

--- SWEA/1961. 숫자 배열 회전/1961.py
+++ SWEA/1961. 숫자 배열 회전/1961.py
@@ -4,6 +4,13 @@ for test_case in range(1, T + 1):
     N = int(input())
     arr = [list(map(int,input().split())) for _ in range(N)]
     
-
+    arr_90 = list(zip(*arr[::-1]))
+    arr_180 = list(zip(*arr_90[::-1]))
+    arr_270 = list(zip(*arr_180[::-1]))
 
     print(f"#{test_case}")
+    for i in range(N):
+        print(''.join(map(str,arr_90[i])), end = ' ')
+        print(''.join(map(str,arr_180[i])), end = ' ')
+        print(''.join(map(str,arr_270[i])), end = ' ')
+        print()
