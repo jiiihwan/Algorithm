34b06e720b7fe2bb5481426a6656525ab9513219
jihwan <yjh2353693@naver.com>
2025-11-21 17:25:00 +0900

    success. 2m

---
+++ SWEA/1986. 지그재그 숫자/1986.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+    ans = 0
+    for i in range(1,N+1):
+        if i % 2 == 0: #짝수
+            ans -= i
+        else:
+            ans += i
+
+    print(f"#{test_case} {ans}")
