79f0c132505dcb13ea790f31f01737e4e47d8bfd
여지환 <yjh2353693@naver.com>
2025-11-25 01:10:14 +0900

    [level 2] Title: 큰 수 만들기, Time: 100.94 ms, Memory: 13.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,11 @@
+def solution(number, k):
+    stack = []
+    for n in number:
+        while stack and k > 0 and stack[-1] < n:
+            stack.pop()
+            k -= 1
+        stack.append(n)
+    ans = ''.join(map(str,stack))
+    if k > 0:
+        ans = ans[:-k]
+    return ans
