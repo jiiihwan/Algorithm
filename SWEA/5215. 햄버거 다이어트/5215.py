c2bb798e899f421101cda8063dc7e23db54b32a6
jihwan <yjh2353693@naver.com>
2026-01-02 01:26:55 +0900

    update

---
+++ SWEA/5215. 햄버거 다이어트/5215.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N,L = map(int, input().split())
+    materials = []
+    for _ in range(N):
+        materials
+
+    print(f"#{test_case} {ans}")
