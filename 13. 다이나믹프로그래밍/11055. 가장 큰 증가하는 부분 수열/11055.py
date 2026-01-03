eb8f0168dd56ca52c5e4ae4a0e1e2fdf909e2559
jihwan <yjh2353693@naver.com>
2026-01-04 00:36:21 +0900

    fail. 45m

---
+++ 13. 다이나믹프로그래밍/11055. 가장 큰 증가하는 부분 수열/11055.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int,input().split()))
+D = [0] * N
+D[0] = A[0]
+mn = A[0]
+
+for i in range(N-1):
+    if A[i] < A[i+1]:
+        D[i+1] = D[i] + A[i+1]
+    elif A[i] >= A[i+1]:
+        if mn <= A[i+1]:
+            D[i+1] = mn + A[i+1]
+        else:
+            D[i+1] = A[i+1]
+        mn = min(mn,A[i])
+
+print(max(D))
