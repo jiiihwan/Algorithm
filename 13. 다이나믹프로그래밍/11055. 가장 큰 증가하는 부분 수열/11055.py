da2673a1757df8a42dd3da9971444d5a2279c8e0
jihwan <yjh2353693@naver.com>
2026-01-05 22:44:34 +0900

    understanding

--- 13. 다이나믹프로그래밍/11055. 가장 큰 증가하는 부분 수열/11055.py
+++ 13. 다이나믹프로그래밍/11055. 가장 큰 증가하는 부분 수열/11055.py
@@ -3,19 +3,17 @@ input = sys.stdin.readline
 
 N = int(input())
 A = list(map(int,input().split()))
-D = [0] * N
-D[0] = A[0]
-mn = A[0]
+#D[i]는 i번째로 끝나는 증가하는 부분수열의 합
+D = A[:] #혼자 시작하는 경우를 생각하여 초기화를 A로 얕은 복사한다.
 
-for i in range(N-1):
-    if A[i] < A[i+1]:
-        D[i+1] = D[i] + A[i+1]
-    elif A[i] >= A[i+1]:
-        if mn <= A[i+1]:
-            D[i+1] = mn + A[i+1]
-        else:
-            D[i+1] = A[i+1]
-        mn = min(mn,A[i])
+#전체 순회
+for i in range(N):
+    #순회한 곳 이전까지 순회하면서
+    for j in range(i):
+        if A[j] < A[i]: #증가하는 부분 수열일 때만 계산, 증가하지 않는 경우는 건너뛴다
+            D[i] = max(D[i], A[i] + D[j])
 
 print(max(D))
-print(D)
+
+#DP의 특성을 잘 생각하자!
+#증가하는 부분수열에 증가하는게 오면 거기다가 추가로붙이면 끝
