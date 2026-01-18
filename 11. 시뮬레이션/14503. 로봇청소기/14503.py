17b6c64554e1f2732555157aac9a3fcbcc6ff6ce
jihwan <yjh2353693@naver.com>
2026-01-19 01:05:37 +0900

    코드 리팩토링

--- 11. 시뮬레이션/14503. 로봇청소기/14503.py
+++ 11. 시뮬레이션/14503. 로봇청소기/14503.py
@@ -12,21 +12,6 @@ r,c,dir = map(int,input().split())
 board = [list(map(int,input().split())) for _ in range(N)]
 vis = [[0]*M for _ in range(N)]
 
-'''
-board = []
-vis = []
-for _ in range(N):
-    a = list(map(int,input().split()))
-    board.append(a)
-    vis.append(a)
-'''
-
-def OOB(i,j):
-    if i < 0 or i >= N or j < 0 or j >= M:
-        return True
-    else:
-        return False
-
 q = deque()
 q.append([r,c])
 vis[r][c] = 1
@@ -50,9 +35,6 @@ while q:
         nx = curX + dx[dir]
         ny = curY + dy[dir]
         if board[nx][ny] == 0: #벽이 아니라 후진가능하면
-            if vis[nx][ny] == 0: #청소 안한 칸이면 청소하기
-                vis[nx][ny] = vis[curX][curY] + 1
-                ans += 1
             q.append([nx,ny])
             dir = (dir+2) % 4 #바라보는 방향 원상복구
         else:
