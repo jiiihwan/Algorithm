cbcf420bb10da242fcef55dc997f70a87c5c44c4
jihwan <yjh2353693@naver.com>
2026-01-22 15:15:59 +0900

    for practice

--- 11. 시뮬레이션/14888. 연산자 끼워넣기/14888.py
+++ 11. 시뮬레이션/14888. 연산자 끼워넣기/14888.py
@@ -2,19 +2,35 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-A = [list(map(int,input().split()))]
+A = list(map(int,input().split()))
 operators = list(map(int,input().split())) #+,-,*,//
 
 max_ans = -1000000000
 min_ans = 1000000000
 
-def calculate(result):
-    global max_ans
-    global min_ans
-    if spent_all:
+def calculate(idx,result): #idx는 깊이, result는 그때까지의 계산결과
+    global max_ans,min_ans
+    if idx == N:
         max_ans = max(max_ans, result)
         min_ans = min(min_ans, result)
         return
-    for num in A:
-        if 
-        calculate(result)
+    for i in range(4):
+        if operators[i] > 0:
+            operators[i] -= 1
+            if i == 0: #'+'
+                calculate(idx+1, result+A[idx])
+            elif i == 1: #'-'
+                calculate(idx+1, result-A[idx])
+            elif i == 2: #'*'
+                calculate(idx+1, result*A[idx])
+            else: # '//'
+                if result < 0:
+                    calculate(idx+1, -(-result//A[idx]))
+                else:
+                    calculate(idx+1, result//A[idx])
+            operators[i] += 1
+
+calculate(1,A[0])
+
+print(max_ans)
+print(min_ans)
