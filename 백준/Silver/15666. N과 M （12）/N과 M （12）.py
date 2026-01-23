7cbd4a6e2ffbe9b26f2e9de91f4dbe0db30f15a1
여지환 <yjh2353693@naver.com>
2026-01-23 10:34:45 +0900

    [Silver II] Title: N과 M (12), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,14 +1,17 @@
 N,M = map(int,input().split())
-nums = sorted(list(set(map(int,input().split()))))
+nums = sorted(list(map(int,input().split())))
 arr = []
 
 def backtracking(start, depth):
     if depth == M:
         print(*arr)
         return
-    for i in range(start,len(nums)):
-        arr.append(nums[i])
-        backtracking(i,depth+1)
-        arr.pop()
-
+    prev = 0
+    for i in range(start,N):
+        if nums[i] != prev:
+            prev = nums[i]
+            arr.append(nums[i])
+            backtracking(i,depth+1)
+            arr.pop()
+	
 backtracking(0,0)
