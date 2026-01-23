025f2cfe4072d987a1230768d2c33aceb57644f4
jihwan <yjh2353693@naver.com>
2026-01-23 15:47:38 +0900

    for practice

--- 10. 백트래킹/1182. 부분수열의 합/1182.py
+++ 10. 백트래킹/1182. 부분수열의 합/1182.py
@@ -1,3 +1,33 @@
+N, S = map(int, input().split())
+nums = list(map(int, input().split()))
+
+ans = 0
+
+def backtracking(idx, total):
+    global ans
+
+    if idx == N:
+        if total == S:
+            ans += 1
+        return
+
+    # 현재 숫자 선택
+    backtracking(idx + 1, total + nums[idx])
+    # 현재 숫자 미선택
+    backtracking(idx + 1, total)
+
+backtracking(0, 0)
+
+# 공집합 제거 (S == 0 인 경우)
+if S == 0:
+    ans -= 1
+
+print(ans)
+
+
+
+
+'''
 N,S = map(int,input().split())
 nums = sorted(list(map(int,input().split())))
 arr = []
@@ -16,4 +46,5 @@ def backtracking(start,depth):
         arr.pop()
 
 backtracking(0,0)
-print(ans)
+print(ans)  
+'''
