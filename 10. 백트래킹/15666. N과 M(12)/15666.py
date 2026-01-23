f8cafd7555459d920741e48a2e2c8e0dd49df19f
jihwan <yjh2353693@naver.com>
2026-01-23 10:49:15 +0900

    add sol

--- 10. 백트래킹/15666. N과 M(12)/15666.py
+++ 10. 백트래킹/15666. N과 M(12)/15666.py
@@ -1,4 +1,23 @@
 N,M = map(int,input().split())
+nums = sorted(list(map(int,input().split())))
+arr = []
+
+def backtracking(start, depth):
+    if depth == M:
+        print(*arr)
+        return
+    prev = 0
+    for i in range(start,N):
+        if nums[i] != prev:
+            prev = nums[i]
+            arr.append(nums[i])
+            backtracking(i,depth+1)
+            arr.pop()
+	
+backtracking(0,0)
+
+'''
+N,M = map(int,input().split())
 nums = sorted(list(set(map(int,input().split()))))
 arr = []
 
@@ -12,3 +31,4 @@ def backtracking(start, depth):
         arr.pop()
 
 backtracking(0,0)
+'''
