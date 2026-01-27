6e4a3a4dbb909553d6931e1bbb088729dfd4ce1d
jihwan <yjh2353693@naver.com>
2026-01-27 10:01:22 +0900

    for practice

--- 10. 백트래킹/16987. 계란으로 계란치기/16987.py
+++ 10. 백트래킹/16987. 계란으로 계란치기/16987.py
@@ -3,49 +3,35 @@ input = sys.stdin.readline
 
 N = int(input())
 S,W = [],[] #내구도, 무게
-broken = [False] * N
 for _ in range(N):
     s,w = map(int,input().split())
     S.append(s)
     W.append(w)
 
-def check_only(cur):
-    global broken
-    cnt = 0
-    for i in range(N):
-        if i != cur and broken[i]:
-            cnt += 1
-    if cnt == N-1:
-        return True
-    else:
-        return False
-
 ans = 0
-def break_egg(cur,depth):
-    print(depth,broken)
+def break_egg(cur):
     global ans,S,W
-    if depth == N:
-        tmp = 0
-        tmp += sum(broken)
-        ans = max(ans, tmp)
+    if cur == N:
+        cnt = sum(1 for x in S if x <= 0)       
+        ans = max(ans, cnt)
+        return
+    if S[cur] <= 0: #이미 깨져있으면 다음으로
+        break_egg(cur+1)
         return
 
+    can_hit = False
     for i in range(N):
-        if i != cur and not broken[i] and not check_only(i):
+        if i != cur and S[i] > 0: #자기 자신을 제외하고 안깨진 경우
+            can_hit = True
+            #계란치기
             S[cur] -= W[i]
             S[i] -= W[cur]
-            if S[cur] <= 0:
-                broken[cur] = True
-            if S[i] <= 0:
-                broken[i] = True
-            break_egg(cur+1, depth+1)
+            break_egg(cur+1)
+            #원상복구
             S[cur] += W[i]
             S[i] += W[cur]
-            if S[cur] > 0:
-                broken[cur] = False
-            if S[i] > 0:
-                broken[i] = False
-        print(broken)
+    if not can_hit: #자기자신을 제외하고 모두 깨졌을 경우
+        break_egg(cur+1)
 
-break_egg(0,1)
+break_egg(0)
 print(ans)
