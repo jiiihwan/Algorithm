cf44cedd5c8008d17457e9def737985b7cf3dc03
여지환 <yjh2353693@naver.com>
2026-01-27 15:45:22 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -4,7 +4,8 @@ for i in range(L):
     ch[i] = ord(ch[i]) - ord('a') + 1
 
 H = 0
+r = 1
 for i in range(L):
-    H += (ch[i] * (31**i)) % 1234567891 
-
+    H = (H + (ch[i] * r)) % 1234567891 
+    r = (r * 31) % 1234567891
 print(H)
