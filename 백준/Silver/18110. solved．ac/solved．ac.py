4d7576df250fcd28b836e05cdadc65344576bc2d
여지환 <yjh2353693@naver.com>
2026-02-02 11:37:41 +0900

    [Silver IV] Title: solved.ac, Time: 136 ms, Memory: 38536 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -9,14 +9,14 @@ for _ in range(n):
 
 difficulty.sort()
 difficulty = deque(difficulty) #큐로 만들기
-cut = int((n * 0.15 + 0.5) // 1) #15% 절사 기준 명수, round대신 사사오입 반올림
+cut = int(n * 0.15 + 0.5)#15% 절사 기준 명수, round대신 사사오입 반올림
 
 for _ in range(cut): #절사
     difficulty.pop()
     difficulty.popleft()
 
 if n != 0:
-    ans = int((sum(difficulty)/len(difficulty) + 0.5) // 1)
+    ans = int(sum(difficulty)/len(difficulty) + 0.5)
 else: 
     ans = 0
 print(ans)
