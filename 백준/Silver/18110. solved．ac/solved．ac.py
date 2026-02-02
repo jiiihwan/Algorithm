dd654faaf24f46597dddf5ff10abf6df77d8cd9b
여지환 <yjh2353693@naver.com>
2026-02-02 11:30:54 +0900

    [Silver IV] Title: solved.ac, Time: 144 ms, Memory: 38536 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+difficulty = []
+n = int(input())
+for _ in range(n):
+    difficulty.append(int(input()))
+
+difficulty.sort()
+difficulty = deque(difficulty) #큐로 만들기
+cut = int((n * 0.15 + 0.5) // 1) #15% 절사 기준 명수, round대신 사사오입 반올림
+
+for _ in range(cut): #절사
+    difficulty.pop()
+    difficulty.popleft()
+
+if n != 0:
+    ans = int((sum(difficulty)/len(difficulty) + 0.5) // 1)
+else: 
+    ans = 0
+print(ans)
