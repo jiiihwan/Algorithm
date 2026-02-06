802d51c23e7baa7c9d253d3e6b9b3811282ed960
jihwan <yjh2353693@naver.com>
2026-02-06 16:23:48 +0900

    [fix] refactoring

--- 백준 CLASS 문제/CLASS3/1389. 케빈 베이컨의 6단계 법칙/1389.py
+++ 백준 CLASS 문제/CLASS3/1389. 케빈 베이컨의 6단계 법칙/1389.py
@@ -2,32 +2,27 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-N,M = map(int,input().split())
-adj = [[] for _ in range(N+1)]
+N, M = map(int, input().split())
+adj = [[] for _ in range(N + 1)]
 for _ in range(M):
-    a,b = map(int,input().split())
-    if b not in adj[a]:
-        adj[a].append(b)
-    if a not in adj[b]:
-        adj[b].append(a)
+    a, b = map(int, input().split())
+    adj[a].append(b)
+    adj[b].append(a)
 
 def bfs(start):
-    q = deque()
-    q.append(start)
-    dist = [1e18]*(N+1)
+    q = deque([start])
+    dist = [-1] * (N + 1)
     dist[start] = 0
+
     while q:
         cur = q.popleft()
         for nxt in adj[cur]:
-            if dist[nxt] < 1e18:
-                continue
+            if dist[nxt] >= 0:
+			          continue 
+            dist[nxt] = dist[cur] + 1
             q.append(nxt)
-            dist[nxt] = min(dist[nxt], dist[cur]+1)
-    #케빈 베이컨 수 구하기
-    ans = 0
-    for i in range(1,N+1):
-        ans += dist[i] 
-    return ans
+
+    return sum(dist[1:])
 
 kevin_bacon = [] 
 for i in range(1,N+1):
