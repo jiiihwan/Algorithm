4ccb09cbe42094dbc6218fb67a663f195aa582da
여지환 <yjh2353693@naver.com>
2026-02-06 12:29:59 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 556 ms, Memory: 41348 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -5,33 +5,35 @@ input = sys.stdin.readline
 dx = [1,0,-1,0]
 dy = [0,1,0,-1]
 
-N,M = map(int,input().split())
+N, M = map(int, input().split())
 board = [list(input().rstrip()) for _ in range(N)]
 vis = [[0]*M for _ in range(N)]
 
 for i in range(N):
     for j in range(M):
         if board[i][j] == 'I':
-            doyeon_i, doyeon_j = i,j
+            doyeon_i, doyeon_j = i, j
             break
+
 ans = 0
 q = deque()
-q.append([doyeon_i,doyeon_j])
+q.append((doyeon_i, doyeon_j))   # ✅ 튜플
 vis[doyeon_i][doyeon_j] = 1
+
 while q:
-    x,y = q.popleft()
+    x, y = q.popleft()
     for d in range(4):
         nx = x + dx[d]
         ny = y + dy[d]
-        if nx < 0 or nx >= N or ny < 0 or ny >=M:
+        if nx < 0 or nx >= N or ny < 0 or ny >= M:
             continue
         if vis[nx][ny] or board[nx][ny] == 'X':
             continue
         elif board[nx][ny] == 'P':
             ans += 1
-        q.append([nx,ny])
+        q.append((nx, ny))      # ✅ 튜플
         vis[nx][ny] = 1
-    
+
 if ans == 0:
     ans = 'TT'
 print(ans)
