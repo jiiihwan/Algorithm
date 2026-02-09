498a493617124ea2dc38c7815c56c02738202e84
여지환 <yjh2353693@naver.com>
2026-02-09 10:59:26 +0900

    [Gold IV] Title: 빙산, Time: 3076 ms, Memory: 35436 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,66 +1,63 @@
 import sys
 from collections import deque
-
-#Recursion limit 설정 (DFS 사용 시 필수)
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-# 상하좌우 방향
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 
-def get_iceberg_count(n, m, grid):
-    visited = [[False] * m for _ in range(n)]
-    count = 0
-    for i in range(n):
-        for j in range(m):
-            if grid[i][j] > 0 and not visited[i][j]:
-                # 새로운 덩어리 발견! BFS 시작
-                count += 1
-                queue = deque([(i, j)])
-                visited[i][j] = True
-                while queue:
-                    x, y = queue.popleft()
+N,M = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(N)]
+
+def solve():
+    def bfs(i,j):
+        q = deque()
+        q.append((i,j))
+        vis[i][j] = 1
+        while q:
+            x,y = q.popleft()
+            for d in range(4):
+                nx = x + dx[d]
+                ny = y + dy[d]
+                if 0<=nx<N and 0<=ny<M:
+                    if board[nx][ny] == 0 or vis[nx][ny]:
+                        continue            
+                    q.append((nx,ny))
+                    vis[nx][ny] = 1
+
+    year = 0
+    while True:
+        vis = [[0]*M for _ in range(N)]
+        chunk = 0
+        for i in range(N):
+            for j in range(M):
+                if board[i][j] != 0 and vis[i][j] == 0: #바다가 아니고 방문 안했을 경우
+                    bfs(i,j)
+                    chunk += 1
+        if chunk == 0: #다 녹은 경우
+            print(0)
+            break
+        elif chunk >= 2:
+            print(year)
+            break
+
+        melt = [[0]*M for _ in range(N)]
+        for i in range(N):
+            for j in range(M):
+                if board[i][j] != 0:
+                    cnt = 0
                     for d in range(4):
-                        nx, ny = x + dx[d], y + dy[d]
-                        if 0 <= nx < n and 0 <= ny < m:
-                            if grid[nx][ny] > 0 and not visited[nx][ny]:
-                                visited[nx][ny] = True
-                                queue.append((nx, ny))
-    return count
+                        nx = i + dx[d]
+                        ny = j + dy[d]
+                        if nx<0 or nx>=N or ny<0 or ny>=M:
+                            continue
+                        if board[nx][ny] == 0:
+                            cnt += 1
+                    melt[i][j] = cnt
+        for i in range(N):
+            for j in range(M):
+                if board[i][j] > 0:
+                    board[i][j] = max(0, board[i][j] - melt[i][j]) 
+        
+        year += 1
 
-def melt_iceberg(n, m, grid):
-    # 각 칸이 얼마나 녹을지 저장
-    melt_amount = [[0] * m for _ in range(n)]
-    for x in range(n):
-        for y in range(m):
-            if grid[x][y] > 0:
-                sea_count = 0
-                for d in range(4):
-                    nx, ny = x + dx[d], y + dy[d]
-                    if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == 0:
-                        sea_count += 1
-                melt_amount[x][y] = sea_count
-    
-    # 실제로 녹이기
-    for x in range(n):
-        for y in range(m):
-            grid[x][y] = max(0, grid[x][y] - melt_amount[x][y])
-
-# 입력 처리
-n, m = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(n)]
-
-year = 0
-while True:
-    count = get_iceberg_count(n, m, grid)
-    
-    if count >= 2: # 덩어리가 2개 이상이면 성공
-        print(year)
-        break
-    if count == 0: # 빙산이 다 녹을 때까지 분리 안 됨
-        print(0)
-        break
-    
-    melt_iceberg(n, m, grid)
-    year += 1
+solve()
