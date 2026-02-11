0ec680aac45953bd0edc3d44d79134ffaadb8173
jihwan <yjh2353693@naver.com>
2026-02-11 23:36:54 +0900

    for practice

--- 16. 이분탐색/2295. 세 수의 합/2295.py
+++ 16. 이분탐색/2295. 세 수의 합/2295.py
@@ -1,23 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-def solve():
-    arr = []
-    N = int(input())
-    for _ in range(N):
-        arr.append(int(input()))
-    arr.sort()
-    for k in range(N-1,-1,-1):
-        for x in range(N):
-            y,z = x,k
-            while x <= y <= z <= k:
-                cnt = arr[x]+arr[y]+arr[z]
-                if cnt == arr[k]:
-                    print(arr[k])
-                    return
-                elif cnt > arr[k]:
-                    z -= 1
-                else:
-                    y += 1
+N = int(input())
+arr = sorted(list(int(input()) for _ in range(N)))
 
-solve()
+# 1. 두 수의 합(a + b) 집합 생성
+sum_of_two = set()
+for i in range(N):
+    for j in range(i, N):
+        sum_of_two.add(arr[i] + arr[j])
+
+# 2. d - c가 집합에 있는지 확인 (큰 값부터 탐색)
+def find_max_k():
+    for i in range(N-1, -1, -1):
+        for j in range(i):
+            if arr[i] - arr[j] in sum_of_two:
+                return arr[i]
+    return -1
+
+print(find_max_k())
