e5ccad7d290441914c466d9e1be5872b37293a05
jihwan <yjh2353693@naver.com>
2026-03-03 01:07:44 +0900

    for practice

---
+++ 11. 시뮬레이션/14888. 연산자 끼워넣기/14888retry.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int,input().split()))
+operators = list(map(int,input().split())) #+,-,*,//
+
+max_ans = -float('inf')
+min_ans = float('inf')
+
+def calculate(idx,result):
+    global max_ans,min_ans
+    if idx == N:
+        max_ans = max(max_ans, result)
+        min_ans = min(min_ans, result)
+        return
+    for i in range(4):
+        if operators[i] > 0:
+            operators[i] -= 1
+            if i == 0: #'+'
+                calculate(idx+1, result+A[idx])
+            elif i == 1: #'-'
+                calculate(idx+1, result-A[idx])
+            elif i == 2: #'*'
+                calculate(idx+1, result*A[idx])
+            else: # '//'
+                if result < 0:
+                    calculate(idx+1, -(-result//A[idx]))
+                else:
+                    calculate(idx+1, result//A[idx])
+
+calculate(1,A[0])
+
+print(max_ans)
+print(min_ans)
